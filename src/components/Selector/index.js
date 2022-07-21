5139018cf171d2d90cb8c5dcd8a5aa6515c64d83
ToppleKek <topkek@topplekek.xyz>
2022-07-21 15:28:20 -0400

    Selector: new custom selector

--- src/components/Selector/index.js
+++ src/components/Selector/index.js
@@ -1,21 +1,84 @@
-import { Component } from 'react';
+import { Component, createRef } from 'react';
 import './Selector.css';
 
-export default class Selector extends Component {
-    value_change = (event) => {
-        this.props.onChange(event.target.value);
+class Option extends Component {
+    on_click = (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        this.props.onSelect(this.props.value);
     }
 
     render() {
-        const options = this.props.options.map((option) =>
-            <option value={option.value} key={option.key}>{option.name}</option>
+        return (
+            <div className='selector-option' key={this.props.key} onClick={this.on_click}>{this.props.name}</div>
         );
+    }
+}
+
+export default class Selector extends Component {
+    constructor(props) {
+        super(props);
+
+        this.parent_ref = createRef();
+        this.options_ref = createRef();
+        this.state = { expanded: false };
+    }
+
+    option_selected = (new_value) => {
+        this.props.onChange(new_value);
+        this.setState({
+            expanded: false,
+        });
+    }
+
+    toggle = () => {
+        this.setState((state) => {
+            return {
+                expanded: !state.expanded
+            };
+        })
+    }
+
+    componentDidMount() {
+        document.addEventListener('mouseup', (event) => {
+            if (this.options_ref.current &&
+                !this.options_ref.current.contains(event.target) &&
+                this.parent_ref.current &&
+                !this.parent_ref.current.contains(event.target)) {
+                this.setState({
+                    expanded: false
+                });
+            }
+        });
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mouseup', this.toggle);
+    }
+
+    render() {
+        const options = this.props.options
+            .filter((option) => option.value !== this.props.value)
+            .map((option) =>
+                <Option key={option.key} value={option.value} name={option.name} onSelect={this.option_selected} />
+            );
+
+        const name = this.props.options.find((option) =>
+            option.value === this.props.value
+        ).name;
 
         return (
-            <div className='generic-select'>
-                <select value={this.props.value} onChange={this.value_change}>
-                    {options}
-                </select>
+            <div className='selector-body'>
+                <div
+                    className={`selector ${this.state.expanded ? 'selector-expanded' : ''}`}
+                    onClick={this.toggle}
+                    ref={this.parent_ref}
+                >
+                    {name}
+                </div>
+                <div className='selector-options' ref={this.options_ref}>
+                    {this.state.expanded ? options : null}
+                </div>
             </div>
         );
     }
