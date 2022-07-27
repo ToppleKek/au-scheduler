c60f1aaeb897520318faf481a1695bd4148d415f
ToppleKek <topkek@topplekek.xyz>
2022-07-27 19:43:58 -0400

    Misc: Add index to components to allow for glob importing

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Button} from './Button';
+export {default as CalendarView} from './CalendarView';
+export {default as Checkbox} from './Checkbox';
+export {default as CourseList} from './CourseList';
+export {default as CourseSearch} from './CourseSearch';
+export {default as InlineButton} from './InlineButton';
+export {default as Scheduler} from './Scheduler';
+export {default as Selector} from './Selector';
